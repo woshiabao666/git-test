e5cdf0dcc191e7dccb876209ff0d372aa4fa4dea
285820415@qq.com <jjh980206>
2022-05-24 15:39:26 +0800

    master commit5

--- src/main/java/com/woshiabao/GitDemo.java
+++ src/main/java/com/woshiabao/GitDemo.java
@@ -3,11 +3,6 @@ package com.woshiabao;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("this is a test!!!");
-        System.out.println("this is a test2!!!");
-        System.out.println("this is a test3!!!");
-        System.out.println("this is a test4!!!");
-        System.out.println("this is a test4!!!");
-        System.out.println("this is a test5!!!");
-        System.out.println("this is a test6!!!");
+
     }
 }
