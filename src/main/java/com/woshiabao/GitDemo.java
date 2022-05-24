68ed358373cecada75a38a24c4afc26bd145f1d7
285820415@qq.com <jjh980206>
2022-05-24 15:12:18 +0800

    second commit

--- src/main/java/com/woshiabao/GitDemo.java
+++ src/main/java/com/woshiabao/GitDemo.java
@@ -3,5 +3,6 @@ package com.woshiabao;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("this is a test!!!");
+        System.out.println("this is a test2!!!");
     }
 }
